149ff8b1b9c7cf7fed2fc217ce26ab66d9bab0c1
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-06-04 17:23:37 +0200

    Building the [id] component

---
+++ components/ChatScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function ChatScreen() {
+  return (
+    <Container>
+      <h1>This is a chat</h1>
+    </Container>
+  )
+}
+
+export default ChatScreen
+
+const Container = styled.div`
+
+`
